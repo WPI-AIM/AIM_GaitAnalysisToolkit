011d607c063b5e482ec47a954df9abca9071ff8b
nathaniel <nagoldfarb@wpi.edu>
2019-07-05 21:48:58 -0400

    EXO-404 force plate class

---
+++ Sensor_suite/ForcePlate.py
@@ -0,0 +1,32 @@
+from Sensor_suite import PostSensor
+
+
+class ForcePlate(PostSensor.PostSensor):
+
+    def __init__(self, name, forces, moments):
+        sensor = {"force": forces, "moment": moments}
+        super(ForcePlate, self).__init__(name, sensor, "IMU")
+
+    def forces(self):
+        return self.sensor["forces"]
+
+    def moments(self):
+        return self.sensor["moments"]
+
+    def get_moment_x(self):
+        return self.moments["Mx"]["data"]
+
+    def get_moment_y(self):
+        return self.moments["My"]["data"]
+
+    def get_moment_z(self):
+        return self.moments["Mz"]["data"]
+
+    def get_force_x(self):
+        return self.forces["Fx"]["data"]
+
+    def get_force_y(self):
+        return self.forces["Fy"]["data"]
+
+    def get_foce_z(self):
+        return self.forces["Fz"]["data"]
