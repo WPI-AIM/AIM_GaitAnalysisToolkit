b91884f373e7af5e502d7c6f7f07a390a4f7eeaf
nathaniel <nagoldfarb@wpi.edu>
2019-07-15 19:23:35 -0400

    working on seperating the joint trajs. Created fnc to get all the locations of the different gait cycles

--- Session/Trial.py
+++ Session/Trial.py
@@ -10,9 +10,40 @@ class Trial(object):
     def __init__(self, vicon_file, config_file=None, exo_file=None, dt=None, notes_file=None):
 
         # self._notes_file = notes_file
-        # self._dt = dt
+        self._dt = 100
         # self._exoskeleton = Exoskeleton.Exoskeleton(config_file, exo_file)
         self._vicon = Vicon.Vicon(vicon_file)
+        self._joint_trajs = None
+        self._black_list = []
+
+
+    def get_index_seperators(self):
+        names = ["HipAngles", "KneeAngles", "AbsAnkleAngle"]
+        offsets = []
+        model = self.vicon.get_model_output()
+        hip = model.get_right_joint("RHipAngles").angle.x
+
+        max_peakind = np.diff(np.sign(np.diff(hip))).flatten()  # the one liner
+        max_peakind = np.pad(max_peakind, (1, 10), 'constant', constant_values=(0, 0))
+        max_peakind = [index for index, value in enumerate(max_peakind) if value == -2]
+
+        for start in xrange(2, len(max_peakind) - 2):
+            error = 10000000
+            offset = 0
+            for ii in xrange(0, 25):
+                temp_error = model.get_left_joint("LKneeAngles").angle.x[max_peakind[start + 1] + ii]
+                if temp_error < error:
+                    error = temp_error
+                    offset = ii
+            offsets.append(offset)
+
+        for side in ("R", "L"):
+            for joint_name in names:
+                name = side + joint_name
+                joints[name] = []
+                for ii, start in enumerate(xrange(2, len(max_peakind) - 2)):
+                    data = (max_peakind[start],max_peakind[start + 1] + offsets[ii])
+                    joints[name].append((data, np.linspace(0, self.dt, len(data))))
 
     def separate_joint_trajectories(self):
 
@@ -43,10 +74,10 @@ class Trial(object):
                 name = side + joint_name
                 joints[name] = []
                 for ii, start in enumerate(xrange(2, len(max_peakind) - 2)):
-                    joints[name].append(
-                        np.array(fnc(name).angle.x[max_peakind[start]:max_peakind[start + 1] + offsets[ii]]))
+                    data = np.array(fnc(name).angle.x[max_peakind[start]:max_peakind[start + 1] + offsets[ii]])
+                    joints[name].append((data, np.linspace(0, self.dt, len(data))))
 
-        return joints
+        self._joint_trajs = joints
 
     @property
     def dt(self):
@@ -60,6 +91,11 @@ class Trial(object):
     def vicon(self):
         return self._vicon
 
+    @property
+    def joint_trajs(self):
+        return self._joint_trajs
+
+
     @dt.setter
     def dt(self, value):
         self._dt = value
@@ -72,15 +108,23 @@ class Trial(object):
     def vicon(self, value):
         self._vicon = value
 
+    @joint_trajs.setter
+    def joint_trajs(self, value):
+        self._joint_trajs = value
+
+
 
 if __name__ == '__main__':
     file = "/home/nathaniel/git/Gait_Analysis_Toolkit/Utilities/Walking01.csv"
     trial = Trial(file)
-    joints = trial.separate_joint_trajectories()
-
+    trial.separate_joint_trajectories()
+    joints = trial.joint_trajs
     fig, ax = plt.subplots()
     index = 0
-    for index in xrange(3):
-        ax.plot(np.arange(len(joints["RHipAngles"][index])), np.radians(joints["RHipAngles"][index]))
+    print joints
+    for index in xrange(1):
+        y = joints["RKneeAngles"][index][0]
+        x = joints["RKneeAngles"][index][1]
+        ax.plot(x,y)
 
     plt.show()
