8504cb45d670476045c7415e9f5740c9cbbc718a
nathanielgoldfarb <nagoldfarb@wpi.edu>
2020-01-30 14:18:11 -0500

    working on DMP

--- DMP/Trainer/GMMTrainer.py
+++ DMP/Trainer/GMMTrainer.py
@@ -1,40 +1,115 @@
 
-from Trainer.TrainerBase import TrainerBase
+import TrainerBase
 import numpy as np
+from scipy import signal
+from Trajectories import utilities as utl
+from Trajectories import GMMWPI
+import numpy as np
+import math
+from lxml import etree
+import scipy.interpolate
 
-class GMMTrainer(TrainerBase):
+class GMMTrainer(TrainerBase.TrainerBase):
 
-    def __init__(self, data, file_name, n_rf, dt):
+    def __init__(self, demo, file_name, n_rf, dt):
         """
            :param file_names: file to save training too
            :param n_rfs: number of DMPs
            :param dt: time step
            :return: None
            """
+        self._kp = 50.0
+        self._kv = (2.0 * self._kp) ** 0.5
 
-        super(GMMTrainer, self).__init__(data, file_name, n_rf, dt)
+        demos = self.resample_demos(demo)
+ 
+        super(GMMTrainer, self).__init__(demos, file_name, n_rf, dt)
 
-    def writeXML(self):
-        pass
 
-    def gen_path(self):
-        pass
+    def writeXML(self, expData, expSigma, H):
+        """
+       Saves the data to a CSV file so that is can be used by a runner
+       :param w: weights from training
+       :param c: gausian centers
+       :param h: Varience of the mean
+       :param y0: startpoint
+       :param goal: goal
+       :return: None
+        """
+
+        expData_st = ["%.6f" % number for number in expData]
+        expSigma_st = ["%.6f" % number for number in expSigma]
+        H_st = ["%.6f" % number for number in H]
+
+        root = etree.Element('RMPs')
+        weights = etree.Element('Weights')
+        inv_sq_var = etree.Element('inv_sq_var')
+        gauss_means = etree.Element('gauss_means')
+        y_start = etree.Element('y0')
+        the_goal = etree.Element('goal')
+
+        the_goal.text = np.str(np.int(goal))
+        y_start.text = np.str(np.int(y0))
+
+        for _w, _c, _h in zip(w_st, c_st, h_st):
+            etree.SubElement(weights, "w").text = _w
+            etree.SubElement(inv_sq_var, "h").text = _h
+            etree.SubElement(gauss_means, "c").text = _c
+
+        root.append(weights)
+        root.append(inv_sq_var)
+        root.append(gauss_means)
+        root.append(the_goal)
+        root.append(y_start)
+        tree = etree.ElementTree(root)
+        tree.write(self._file_name, pretty_print=True, xml_declaration=True, encoding="utf-8")
+
 
     def train(self):
-        pass
+        """
 
-    def getTraj(demos, samples, nbData_):
-        nbData = nbData_  # Length of each trajectory
-        dt = 0.01
-        kp = 50.0
-        kv = (2.0 * kp) ** 0.5
+        """
+        nb_dim = len(self._demo)
+        gmm = GMMWPI.GMMWPI(nb_states=self._n_rfs, nb_dim=nb_dim)
+        tau, motion, sIn = self.gen_path(self._demo)
+        gmm.init_params_kmeans(tau)
+        gmm.em(tau, no_init=True)
+        expData, expSigma, H = gmm.gmr(sIn, [0], [1])
+
+    @staticmethod
+    def resample_demos(self, trajs):
+        # find resample length to use
+        resample = 100000
+        for traj in trajs:
+            sample = len(traj)
+            resample = min(resample, sample)
+
+        demos = []
+        for traj in trajs:
+            data = signal.resample(traj, resample)
+            temp = []
+            for d in data:
+                temp.append([np.array(d)])
+            temp = np.array(temp)
+            demos.append(temp)
+
+        return demos
+
+
+    def gen_path(self, demos):
+        """
+
+        """
+
+        nbData =len(demos[0])
+        samples = len(demos)
+
         alpha = 1.0
         x_ = None
         dx_ = None
         ddx_ = None
         sIn = []
         taux = []
-        tauy = []
 
         sIn.append(1.0)  # Initialization of decay term
         for t in xrange(1, nbData):
@@ -46,12 +121,12 @@ class GMMTrainer(TrainerBase):
             demo = demos[n]
             size = demo.shape[0]
             x = utl.spline(np.arange(1, size + 1), demo, np.linspace(1, size, nbData))
-            dx = np.divide(np.diff(x, 1), np.power(dt, 1.0))
+            dx = np.divide(np.diff(x, 1), np.power(self._dt, 1.0))
             dx = np.append([0.0], dx[0])
-            ddx = np.divide(np.diff(x, 2), np.power(dt, 2))
+            ddx = np.divide(np.diff(x, 2), np.power(self._dt, 2))
             ddx = np.append([0.0, 0.0], ddx[0])
             goals = np.matlib.repmat(goal, nbData, 1)
-            tau_ = ddx - (kp * (goals.transpose() - x)) / sIn + (kv * dx) / sIn
+            tau_ = ddx - (self._kp * (goals.transpose() - x)) / sIn + (self._kv * dx) / sIn
 
             if x_ is not None:
                 x_ = x_ + x.tolist()
--- DMP/Trainer/TrainerBase.py
+++ DMP/Trainer/TrainerBase.py
@@ -3,14 +3,14 @@ import abc
 
 class TrainerBase(object):
 
-    def __init__(self, data, file_name, n_rfs, dt):
+    def __init__(self, demo, file_name, n_rfs, dt):
         """
            :param file_names: file to save training too
            :param n_rfs: number of DMPs
            :param dt: time step
            :return: None
            """
-        self._data = data
+        self._demo = demo
         self._file_name = file_name
         self._n_rfs = n_rfs
         self._dt = dt
