74a965f545cbb0a42852c114b2c98772091174c1
nathanielgoldfarb <nagoldfarb@wpi.edu>
2020-03-05 11:53:02 -0500

    working on model

--- Model/LowerLimb.py
+++ Model/LowerLimb.py
@@ -8,22 +8,36 @@ class LowerLimb(Model.Model):
         super(LowerLimb, self).__init__(mass, height)
         self.make_body()
 
-    def IK(self, toe, hip_location=[0,0], ankle_contraint=False):
+    def IK(self, toe, offset=[0, 0], ankle_contraint=False):
 
-        x = toe[0] + hip_location[0]
-        y = toe[1] + hip_location[1]
-        l1 = self.lengths["thigh"]
-        l2 = self.lengths["shank"]
+        l1 = -self.lengths["thigh"]
+        l2 = -self.lengths["shank"]
         l3 = self.lengths["ankle"]
         l4 = self.lengths["foot_length"]
 
-        num = x*x + y*y - l1**2 - l2**2
-        dem = 2*l1*l2
+        x = toe[0] + offset[0] - l4
+        y = toe[1] + offset[1] + l3
 
-        q2 = np.arccos(num / dem)
+        num = x * x + y * y - l1 ** 2 - l2 ** 2
+        dem = 2 * l1 * l2
+        print num / dem
+        q2 = np.arctan2(-np.sqrt(1 - (num / dem) * (num / dem)), (num / dem))
+        q1 = np.arctan2(y, x) - np.arctan2(l2 * np.sin(q2), l1 + l2 * np.cos(q2))
+        q3 = 0.5 * np.pi - q1 - q2
+        return [q1, q2, q3]
 
-        q1 = np.arctan2(y, x) - np.arctan2( l2*np.sin(q2), l1 + l2*np.cos(q2))
-        q3 = np.pi - q1 - q2
+    def FK(self, q, offset=np.array([0,0])):
+
+        l1 = -self.lengths["thigh"]
+        l2 = -self.lengths["shank"]
+        l3 = -self.lengths["ankle"]
+        l4 = self.lengths["foot_length"]
+
+        P1 = offset + np.array([l1 * np.cos(q[0]), l1 * np.sin(q[0])])
+        P2 = P1 + np.array([l2 * np.cos(q[0] + q[1]), l2 * np.sin(q[0] + q[1])])
+        P3 = P2 + np.array([l3 * np.cos(q[0] + q[1] + q[2]), l3 * np.sin(q[0] + q[1] + q[2])])
+        P4 = P3 + np.array([l4 * np.cos((q[0] + q[1] + q[2]) - 0.5 * np.pi), l4 * np.sin((q[0] + q[1] + q[2]) - 0.5 * np.pi)])
+        joint = np.array([offset, P1, P2, P3, P4])
+        return joint
 
 
-        pass
