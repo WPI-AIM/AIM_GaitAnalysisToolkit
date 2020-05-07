284925c7f1f05d388db4f49250fb51176ba6a330
nathaniel <nagoldfarb@wpi.edu>
2020-05-07 11:16:00 -0400

    making model for GMM abstracting

---
+++ LearningTools/Models/GMM.py
@@ -0,0 +1,236 @@
+from termcolor import colored
+import numpy as np
+import copy
+import matplotlib
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import ModelBase
+
+class GMM(ModelBase.ModelBase):
+
+    def __init__(self, nb_states, nb_dim=3, init_zeros=False, mu=None, lmbda=None, sigma=None, priors=None):
+
+        super(GMM, self).__init__(nb_states, nb_dim, init_zeros, mu, lmbda, sigma, priors)
+
+    def init_params(self, data):
+
+        idList = self.kmeansclustering(data)
+        priors = np.ones(self.nb_states) / self.nb_states
+        self._sigma = np.array([np.eye(self.nb_dim) for i in range(self.nb_states)])
+
+        for i in xrange(self.nb_states):
+
+            idtmp = np.where(idList == i)
+            mat = np.vstack((data[:, idtmp][0][0], data[:, idtmp][1][0]))
+
+            for j in xrange(2, len(data[:, idtmp])):
+                mat = np.vstack((mat, data[:, idtmp][j][0]))
+
+            mat = np.concatenate((mat, mat), axis=1)
+            priors[i] = len(idtmp[0])
+            self._sigma[i] = np.cov(mat) + np.eye(self.nb_dim) * self.reg
+
+        self._priors = priors / np.sum(priors)
+
+    def train(self, data, reg=1e-8, maxiter=2000):
+
+        gamma, BIC = self.em(data, reg, maxiter)
+        return gamma, BIC
+
+    def get_model(self):
+        return self.sigma, self.mu
+
+    def kmeansclustering(self, data, reg=1e-8):
+
+        # Criterion to stop the EM iterative update
+        cumdist_threshold = 1e-10
+        maxIter = 200
+        minIter = 20
+
+        # Initialization of the parameters
+        cumdist_old = -1.7977e+308
+        nb_step = 0
+        self.nbData = data.shape[1]
+        id_tmp = np.random.permutation(self.nbData)
+
+        Mu = copy.deepcopy(data[:, id_tmp[:self.nb_states]])
+        searching = True
+        distTmpTrans = np.zeros((len(data[0]), self.nb_states,))
+        idList = []
+
+        while searching:
+
+            # E-step %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %%
+            for i in xrange(0, self.nb_states):
+                # Compute distances
+                thing = np.matlib.repmat(Mu[:, i].reshape((-1, 1)), 1, self.nbData)
+                temp = np.power(data - thing, 2.0)
+                temp2 = np.sum(temp, 0)
+                distTmpTrans[:, i] = temp2
+
+            vTmp = np.min(distTmpTrans, 1)
+            cumdist = sum(vTmp)
+            idList = []
+
+            for row, min_num in zip(distTmpTrans, vTmp):
+                index = np.where(row == min_num)[0]
+                idList.append(index[0])
+
+            idList = np.array(idList)
+            # M-step %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            for i in xrange(self.nb_states):
+                # Update the centers
+                id = np.where(idList == i)
+                Mu[:, i] = np.mean(data[:, id], 2).reshape((1, -1))
+
+            # Stopping criterion %%%%%%%%%%%%%%%%%%%%
+            if abs(cumdist - cumdist_old) < cumdist_threshold and nb_step > minIter:
+                print 'Maximum number of kmeans iterations, ' + str(abs(cumdist - cumdist_old)) + ' is reached'
+                print 'steps reached, ' + str(nb_step) + ' is reached'
+                searching = False
+
+            cumdist_old = cumdist
+            nb_step = nb_step + 1
+
+            if nb_step > maxIter:
+                print 'steps reached, ' + str(nb_step) + ' is reached'
+                searching = False
+            print "maxitter ", nb_step
+
+        self.mu = Mu
+
+        return idList
+
+
+
+    def em(self, data, reg=1e-8, maxiter=2000):
+
+        self.reg = reg
+
+        nb_min_steps = 50  # min num iterations
+        nb_max_steps = maxiter  # max iterations
+        nb_samples = data.shape[1]
+
+        data = data.T
+        searching = True
+        LL = np.zeros(nb_max_steps)
+        it = 0
+        GAMMA = None
+        while searching:
+
+            # E - step
+            L = np.zeros((self.nb_states, nb_samples))
+
+            for i in range(self.nb_states):
+                L[i, :] = self.priors[i] * self.gaussPDF(data.T, self.mu[:, i], self.sigma[i])
+
+            GAMMA = L / np.sum(L, axis=0)
+            GAMMA2 = GAMMA / np.sum(GAMMA, axis=1)[:, np.newaxis]
+
+            # M-step
+            for i in xrange(self.nb_states):
+                # update priors
+                self.priors[i] = np.sum(GAMMA[i, :]) / self.nbData
+                self.mu[:, i] = data.T.dot(GAMMA2[i, :].reshape((-1, 1))).T
+                mu = np.matlib.repmat(self.mu[:, i].reshape((-1, 1)), 1, self.nbData)
+                diff = (data.T - mu)
+                self.sigma[i] = diff.dot(np.diag(GAMMA2[i, :])).dot(diff.T) + np.eye(self.nb_dim) * self.reg
+
+            # self.priors = np.mean(GAMMA, axis=1)
+
+            LL[it] = np.sum(np.log(np.sum(L, axis=0))) #/ self.nbData
+            # Check for convergence
+            if it > nb_min_steps:
+                if LL[it] - LL[it - 1] < 0.00001 or it == (maxiter - 1):
+                    searching = False
+
+            it += 1
+
+        self.BIC = self.BIC_score(LL[it-1])
+        return GAMMA, self.BIC
+
+    def gmr(self, DataIn, in_, out_):
+
+        nbData = np.shape(DataIn)[0]
+        nbVarOut = len(out_)
+        in_ = in_[0]
+
+        MuTmp = np.zeros((nbVarOut, self.nb_states))
+        expData = np.zeros((nbVarOut, nbData))
+        expSigma = []
+        for i in xrange(nbData):
+            expSigma.append(np.zeros((nbVarOut, nbVarOut)))
+
+        H = np.zeros((self.nb_states, nbData))
+
+        for t in xrange(nbData):
+
+            for i in xrange(self.nb_states):
+                H[i, t] = self.priors[i] * self.gaussPDF(np.asarray([DataIn[t]]),
+                                                         self.mu[in_][i],
+                                                         self.sigma[i][in_, in_])
+
+            H[:, t] = H[:, t] / np.sum(H[:, t] + np.finfo(float).tiny)
+
+            for i in xrange(self.nb_states):
+                MuTmp[:, i] = self.mu[out_, i] + self.sigma[i][out_, in_] / \
+                              self.sigma[i][in_, in_] * \
+                              (DataIn[t] - self.mu[in_, i])
+
+                expData[:, t] = expData[:, t] + H[i, t] * MuTmp[:, i]
+
+            for i in xrange(self.nb_states):
+                sigma_tmp = self.sigma[i][out_[0]:(out_[-1] + 1), out_[0]:(out_[-1] + 1)] - \
+                            self.sigma[i][out_, in_] / self.sigma[i][in_, in_] * self.sigma[i][in_, out_]
+
+                expSigma[t] = expSigma[t] + H[i, t] * (sigma_tmp + MuTmp[:, i].reshape((-1, 1)) * MuTmp[:, i].T)
+
+            expSigma[t] = expSigma[t] - expData[:, t] * expData[:, t].T + np.eye(nbVarOut) * 1E-8
+
+        return expData, expSigma, H
+
+    def gaussPDF(self, x, mean, covar):
+        '''Multi-variate normal distribution
+
+        x: [n_data x n_vars] matrix of data_points for which to evaluate
+        mean: [n_vars] vector representing the mean of the distribution
+        covar: [n_vars x n_vars] matrix representing the covariance of the distribution
+
+        '''
+
+        # Check dimensions of covariance matrix:
+        if type(covar) is np.ndarray:
+            n_vars = covar.shape[0]
+        else:
+            n_vars = 1
+
+        # Check dimensions of data:
+        if x.ndim > 1 and n_vars == len(x):
+            nbData = x.shape[1]
+        else:
+            nbData = x.shape[0]
+
+        # nbData = x.shape[1]
+        mu = np.matlib.repmat(mean.reshape((-1, 1)), 1, nbData)
+        diff = (x - mu)
+
+        # Distinguish between multi and single variate distribution:
+        if n_vars > 1:
+            lambdadiff = np.linalg.pinv(covar).dot(diff) * diff
+            scale = np.sqrt(
+                np.power((2 * np.pi), n_vars) * (abs(np.linalg.det(covar)) + 2.2251e-308))
+            p = np.sum(lambdadiff, 0)
+        else:
+            lambdadiff = diff / covar
+            scale = np.sqrt(np.power((2 * np.pi), n_vars) * covar + 2.2251e-308)
+            p = diff * lambdadiff
+
+        prop = np.exp(-0.5 * p) / scale
+        return prop.T
+
+
+
+
+
+# p = PatchCollection(patches)
+# ax.add_collection(p)
---
+++ LearningTools/Models/ModelBase.py
@@ -0,0 +1,136 @@
+from termcolor import colored
+import numpy as np
+import copy
+import matplotlib
+from matplotlib.patches import Polygon
+import abc
+
+from matplotlib.collections import PatchCollection
+
+
+class ModelBase(object):
+
+    def __init__(self, nb_states, nb_dim=2, init_zeros=False, mu=None, lmbda=None, sigma=None, priors=None):
+
+        # gmm.GMM.__init__(self, nb_states=nb_states, nb_dim=nb_dim)
+        self._nb_dim = nb_dim
+        self._nb_states = nb_states
+        # flag to indicate that publishing was not init
+        self.publish_init = False
+        self._mu = mu
+        self._lmbda = lmbda
+        self._sigma = sigma
+        self._priors = priors
+        self._nbData = 0
+        self._reg = 1e-8
+        self._nbData = 0
+
+        if init_zeros:
+            self.init_zeros()
+
+    @abc.abstractmethod
+    def init_params(self, data):
+        pass
+
+    @abc.abstractmethod
+    def train(self, data, reg=1e-8, maxiter=2000):
+        pass
+
+    @abc.abstractmethod
+    def get_model(self):
+        pass
+
+    @property
+    def nb_dim(self):
+        return self._nb_dim
+
+    @nb_dim.setter
+    def nb_dim(self, value):
+        self._nb_dim = value
+
+    @property
+    def nb_states(self):
+        return self._nb_states
+
+    @nb_states.setter
+    def nb_states(self, value):
+        self._nb_states = value
+
+    @property
+    def mu(self):
+        return self._mu
+
+    @mu.setter
+    def mu(self, value):
+        self._nb_dim = value.shape[0]
+        self._nb_states = value.shape[1]
+        self._mu = value
+
+    @property
+    def priors(self):
+        return self._priors
+
+    @priors.setter
+    def priors(self, value):
+        self._priors = value
+
+    @property
+    def sigma(self):
+        return self._sigma
+
+    @sigma.setter
+    def sigma(self, value):
+        self._sigma = value
+
+    @property
+    def nbData(self):
+        return self._nbData
+
+    @nbData.setter
+    def nbData(self, value):
+        self._nbData = value
+
+    def BIC_score(self, LL):
+        """
+        calculate the BIC score
+        :param LL:
+        :return:
+        """
+        N = self._nbData
+        D = self.nb_dim
+        K = self.nb_states
+        n_p = (K - 1) + K * (D + 0.5 * D * (D + 1))
+        return -LL + 0.5 * n_p * np.log(N)
+
+    def AIC_score(self, LL):
+        """
+        calculate the BIC score
+        :param LL:
+        :return:
+        """
+        N = self._nbData
+        D = self.nb_dim
+        K = self.nb_states
+        n_p = (K - 1) + K * (D + 0.5 * D * (D + 1))
+        return -np.log(np.power(LL,2)) + 2*K
+
+
+def plot_activation(sIn, H, ax):
+    nbDrawingSeg = 50
+    t = np.linspace(-np.pi, np.pi, nbDrawingSeg)
+    nb_states = len(H)
+    patches = []
+    sIn_ = sIn
+    sIn_.insert(0, 0)
+    sIn_.append(0)
+
+    for i in xrange(4):
+        h = H[i].tolist()
+        h.insert(0, 0)
+        h.append(0)
+        fn = map(list, zip(*[sIn_, h]))
+        # patches.append(Polygon(fn,fill=None, edgecolor='r'))
+        ax.add_patch(Polygon(fn, fill=None, edgecolor='r'))
+
+# p = PatchCollection(patches)
+# ax.add_collection(p)
