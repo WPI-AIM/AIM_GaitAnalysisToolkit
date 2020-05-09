959ef2b89575e3144b1e31f6c1605ee8b1fa4a7f
nathaniel <nagoldfarb@wpi.edu>
2020-05-08 23:53:10 -0400

    trainner and runner verified working, need to write PrpductTMGMM

--- LearningTools/Trainer/TPGMMTrainer.py
+++ LearningTools/Trainer/TPGMMTrainer.py
@@ -56,7 +56,6 @@ class TPGMMTrainer(TrainerBase.TrainerBase):
         return self.data
 
 
-
     def solve_riccati(self, expSigma):
         Ad = np.eye(2)
         Q = np.zeros((2,2))
@@ -68,17 +67,39 @@ class TPGMMTrainer(TrainerBase.TrainerBase):
 
         for ii in xrange(len(expSigma)-2, -1, -1):
             Q[0,0] = np.linalg.pinv(expSigma[ii])
-            B = np.dot(P[ii+1],Bd)
-            C = np.linalg.pinv( np.dot( np.dot(Bd.T ,P[ii+1]), Bd) + R)
-            D = np.dot( Bd.T ,P[ii+1])
-            F = np.dot(np.dot(Ad.T, np.dot(np.dot(B, C), D) - P[ii+1]), Ad)
+            B = P[ii + 1] * Bd
+            C = np.linalg.pinv(np.dot(Bd.T * P[ii + 1], Bd) + R)
+            D = Bd.T * P[ii + 1]
+            F = np.dot(np.dot(Ad.T, B * C * D - P[ii + 1]), Ad)
             P[ii] = Q - F
-
 
         self.data["Ad"] = Ad
         self.data["Bd"] = Bd
         self.data["R"] = R
         self.data["P"] = P
+
+    # def solve_riccati(self, expSigma):
+    #     Ad = np.eye(2)
+    #     Q = np.zeros((2,2))
+    #     Bd = np.array([[0],[self._dt]])
+    #     Ad[0,1] = self._dt
+    #     R = np.eye(1)*self.gmm.reg
+    #     P = [np.zeros((2, 2))] * len(expSigma)
+    #     P[-1][0, 0] = np.linalg.pinv(expSigma[-1])
+    #
+    #     for ii in xrange(len(expSigma)-2, -1, -1):
+    #         Q[0,0] = np.linalg.pinv(expSigma[ii])
+    #         B = P[ii+1]*Bd
+    #         C = np.linalg.pinv(  Bd.T  * P[ii+1] * Bd + R)
+    #         D =  Bd.T  * P[ii+1]
+    #         F = np.dot(np.dot(Ad.T, B * C * D - P[ii+1]), Ad)
+    #         P[ii] = Q - F
+    #
+    #
+    #     self.data["Ad"] = Ad
+    #     self.data["Bd"] = Bd
+    #     self.data["R"] = R
+    #     self.data["P"] = P
 
     def gen_path(self, demos):
         """
