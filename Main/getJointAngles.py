88bb2a2d402801e46e1706ad6f7fa7e7fc5a4167
nathaniel <nagoldfarn@wpi.edu>
2020-06-02 15:07:00 -0400

    plots joint angles

--- Main/getJointAngles.py
+++ Main/getJointAngles.py
@@ -10,20 +10,6 @@ from scipy import signal
 import numpy.polynomial.polynomial as poly
 from dtw import dtw
 
-def refit(x, y):
-    manhattan_distance = lambda x, y: np.abs(x - y)
-
-    d, cost_matrix, acc_cost_matrix, path = dtw(x, y, dist=manhattan_distance)
-    data_warp = [y[path[1]][:x.shape[0]]]
-    t = np.linspace(0, 1.0, len(data_warp[0]))
-    coefs = poly.polyfit(t, data_warp[0], 20)
-    ffit = poly.Polynomial(coefs)  # instead of np.poly1d
-    y_fit = ffit(t)
-    y_fit = data_warp[0]
-    temp = [[np.array(ele)] for ele in y_fit.tolist()]
-    return np.array(temp)
-
-
 
 def plot_joint_angles(files, indecies, sides, lables):
     """
@@ -56,45 +42,56 @@ def plot_joint_angles(files, indecies, sides, lables):
         angles["knee"].append(knee_angle)
         angles["ankle"].append(ankle_angle)
         samples.append(len(hip_angle))
+    SMALL_SIZE = 10
+    MEDIUM_SIZE = 10
+    BIGGER_SIZE = 18
 
-    plt.figure(1)
+    plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=BIGGER_SIZE)  # fontsize of the axes title
+    plt.rc('axes', labelsize=BIGGER_SIZE)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=BIGGER_SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=BIGGER_SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
     f, (ax1_angle, ax2_angle, ax3_angle) = plt.subplots(3, 1)
-    sample_size = max(samples)
-    index = samples.index(sample_size)
+    sample_size = min(samples)
+
+    ax1_angle.set_title("Hip Angle")
+    ax2_angle.set_title("Knee Angle")
+    ax3_angle.set_title("Ankle Angle")
+    f.suptitle('Walking Joint Angles', fontsize=20)
+
+    ax1_angle.set_ylabel("Angle (Degrees)")
+    ax2_angle.set_ylabel("Angle (Degrees)")
+    ax3_angle.set_ylabel("Angle (Degrees)")
+    t = np.linspace(0,100,sample_size)
 
     for i in range(len(files)):
-        ax1_angle.plot(angles["hip"][i])
-        ax2_angle.plot(angles["knee"][i])
-        ax3_angle.plot(angles["ankle"][i])
+        ax1_angle.plot(t, signal.resample(angles["hip"][i], sample_size))
+        ax2_angle.plot(t, signal.resample(angles["knee"][i], sample_size))
+        ax3_angle.plot(t, signal.resample(angles["ankle"][i], sample_size))
+
+    plt.xlabel("Gait %")
+    plt.legend(lables)
 
     plt.show()
 
 
 if __name__ == "__main__":
-    plot_joint_angles(["/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_10/subject_10 walk_02.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_00/subject_00 walk_00.csv",
+    plot_joint_angles(["/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_00/subject_00 walk_00.csv",
+                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_01/subject_01_walk_00.csv",
+                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_02/subject_02_walk_00.csv",
                        "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_03/subject_03_walk_00.csv",
+                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_04/subject_04_walk_00.csv",
+                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_05/subject_05_walk_00.csv",
                        "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_06/subject_06 walk_00.csv",
                        "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_07/subject_07 walk_01.csv",
+                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_08/subject_08_walking_01.csv",
+                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_10/subject_10 walk_02.csv",
                        ],
-                      [2, 1, 0, 2, 0],
-                      ["L", "L", "L", "L", "L"],
-                      ["Subject10", "Subject00"])
+                      [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 2],
+                      ["R", "R", "R", "R", "R", "R", "R", "R","R", "R" ],
+                      ["Subject00", "Subject01", "Subject02",  'Subject03', "subject04", "Subject05", "Subject06", "Subject07",  "Subject08", "Subject10"])
 
 
 
-    ["/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_00/subject_00 walk_00.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_01/subject_01_walk_00.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_02/subject_02_walk_00.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_03/subject_03_walk_00.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_04/subject_04_walk_00.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_05/subject_05_walk_00.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_06/subject_06 walk_00.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_07/subject_07 walk_00.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_08/subject_08_walking_01.csv",
-     "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_10/subject_10 walk_00.csv"]
-
-    [1, 8, 16, 2, 11, 4, 4, 11, 16, 9, 9]
-
-    ["Subject0", "Subject1", "Subject2", "Subject3", "Subject4", "Subject5", "Subject6", "Subject7", "Subject8",
-     "Subject10"]
