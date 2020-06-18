05751db983de1f843caa4cbb890d662b6e07a957
ajlewis02 <ajlewis@wpi.edu>
2020-06-18 11:57:49 -0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setuptools.setup(
     name="GaitAnalysisToolkit",
     version="1.0",
     install_requires=[
-        "GaitCore @ git+https://github.com/WPI_AIM/AIM_GaitCore.git",
-        "Vicon @ git+https://github.com/WPI_AIM/AIM_Vicon.git"
+        "GaitCore @ git+https://github.com/WPI-AIM/AIM_GaitCore.git",
+        "Vicon @ git+https://github.com/WPI-AIM/AIM_Vicon.git"
     ],
     packages=setuptools.find_packages(exclude=["testing", "Examples"])
 )
