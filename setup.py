ec0e3c51b7cc91439216f55ea15a9bb1957059de
ajlewis02 <ajlewis@wpi.edu>
2020-06-18 12:04:20 -0400

    Added further dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setuptools.setup(
     version="1.0",
     install_requires=[
         "GaitCore @ git+https://github.com/WPI-AIM/AIM_GaitCore.git",
-        "Vicon @ git+https://github.com/WPI-AIM/AIM_Vicon.git"
+        "Vicon @ git+https://github.com/WPI-AIM/AIM_Vicon.git",
+        "pandas",
+        "numpy",
+        "scipy",
+        "dtw",
+        "matplotlib"
     ],
     packages=setuptools.find_packages(exclude=["testing", "Examples"])
 )
