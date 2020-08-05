0253c32fff49a6549956bea5705484381902433e
nagoldfarb <nagoldfarb@wpi.edu>
2020-08-05 07:55:44 -0400

    reseting setup pulls

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setuptools.setup(
     name="GaitAnalysisToolkit",
     version="1.1.1",
     install_requires=[
-        "GaitCore @ git+https://github.com/nag92/AIM_GaitCore.git",
-        "Vicon @ git+https://github.com/nag92/AIM_Vicon.git",
+        "GaitCore @ git+https://github.com/WPI-AIM/AIM_GaitCore.git",
+        "Vicon @ git+https://github.com/WPI-AIM/AIM_Vicon.git",
         "pandas",
         "numpy",
         "scipy",
